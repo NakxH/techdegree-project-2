1e1e55372714b9bda6d2b7d7f5f5a85b42c59ec1
Hakan <74737999+NakxH@users.noreply.github.com>
2021-08-22 22:20:23 +0100

    Added 'Exceeded' criteria to the project

--- js/script.js
+++ js/script.js
@@ -11,11 +11,123 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
-const headerSection = document.querySelector('.header');
 
-const searchLabel = document.createElement('label');
-searchLabel.className = 'student-search'
-headerSection.appendChild(searchLabel);
+
+// Declared in the global Scope as they are used in multiple functions
+const studentList = document.querySelector('.student-list');
+const linkList = document.querySelector('.link-list');
+
+
+
+/*
+Exceeded cirteria:
+   Here I have created the Input element that allows a user to search.
+*/
+
+
+
+function createSearch () {
+
+   const headerSection = document.querySelector('.header');
+
+   const searchLabel = document.createElement('label');
+   searchLabel.className = 'student-search';
+   searchLabel.setAttribute('for', 'search');
+   headerSection.appendChild(searchLabel);
+   
+   const searchText = document.createElement('span');
+   searchText.textContent = 'Search by name';
+   searchLabel.appendChild(searchText);
+   
+   const searchInput = document.createElement('input');
+   searchInput.type = 'input';
+   searchInput.id = 'search';
+   searchInput.placeholder = 'Search by Name...';
+   searchLabel.appendChild(searchInput);
+   
+   const searchButton = document.createElement('button');
+   searchButton.type = 'button';
+   searchLabel.appendChild(searchButton);
+   
+   const searchBtnImg = document.createElement('img');
+   searchBtnImg.src = 'img/icn-search.svg';
+   searchBtnImg.alt = 'Search Icon'
+   searchButton.appendChild(searchBtnImg);
+
+   headerSection.addEventListener('keyup', (e) => {
+      e.preventDefault();
+      studentSearch(e.target.value);
+   });
+   
+   headerSection.addEventListener('click', (e) => {
+      e.preventDefault();
+      const input = document.querySelector('#search');
+      studentSearch(input.value);
+   });
+}
+
+
+
+function studentSearch (search) {
+
+   studentList.innerHTML = '';
+
+   for ( let i = 0; i < data.length; i++ ) {
+
+// We use Object.values to provide an array from the name object so we can work with an array.
+
+      const nameArray = Object.values(data[i].name);
+      nameArray.shift();
+
+      const name = nameArray.join(' ').toLowerCase();
+
+      if ( search && name.includes(search.toLowerCase()) ) {
+
+         createStudent(data[i]);
+
+      }
+   }
+}
+
+
+
+function createStudent (item) {
+
+   const student = document.createElement('li');
+   student.className = 'student-item cf';
+   
+   const details = document.createElement('div');
+   details.className = 'student-details';
+   student.appendChild(details);
+
+   const img = document.createElement('img');
+   img.className = 'avatar';
+   img.src = item.picture.thumbnail;
+   img.alt = 'Profile Picture'
+   details.appendChild(img);
+
+   const name = document.createElement('h3')
+   name.textContent = `${item.name.first} ${item.name.last}`;
+   details.appendChild(name);
+
+   const email = document.createElement('span');
+   email.className = 'email';
+   email.textContent = `${item.email}`;
+   details.appendChild(email);
+
+   const joinedDetails = document.createElement('div');
+   joinedDetails.className = 'joined-details';
+   student.appendChild(joinedDetails);
+
+   const joinDate = document.createElement('span');
+   joinDate.className = 'date'
+   joinDate.textContent = `Joined ${item.registered.date}`;
+   joinedDetails.appendChild(joinDate);
+
+   studentList.appendChild(student);
+
+}
+
 
 
 /*
@@ -23,62 +135,36 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
+
+
 function showPage (list, page) {
 
    const startIndex = (page * 9) - 9;
    const endIndex = page * 9;
-   const studentList = document.querySelector('.student-list');
    studentList.innerHTML = '';
    
    for ( let i = 0; i < list.length; i++ ) {
 
       if ( i >= startIndex && i < endIndex ) {
-         const student = document.createElement('li');
-         student.className = 'student-item cf';
-         
-         const details = document.createElement('div');
-         details.className = 'student-details';
-         student.appendChild(details);
 
-         const img = document.createElement('img');
-         img.className = 'avatar';
-         img.src = list[i].picture.thumbnail;
-         img.alt = 'Profile Picture'
-         details.appendChild(img);
-
-         const name = document.createElement('h3')
-         name.textContent = `${list[i].name.first} ${list[i].name.last}`;
-         details.appendChild(name);
-
-         const email = document.createElement('span');
-         email.className = 'email';
-         email.textContent = `${list[i].email}`;
-         details.appendChild(email);
-
-         const joinedDetails = document.createElement('div');
-         joinedDetails.className = 'joined-details';
-         student.appendChild(joinedDetails);
-
-         const joinDate = document.createElement('span');
-         joinDate.className = 'date'
-         joinDate.textContent = `Joined ${list[i].registered.date}`;
-         joinedDetails.appendChild(joinDate);
-
-         studentList.appendChild(student);
+         createStudent(list[i]);
 
       }
    }
 }
+
+
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
+
+
 function addPagination (list) {
 
    let pageCount = list.length / 9;
-   const linkList = document.querySelector('.link-list');
    linkList.innerHTML = '';
 
    for ( let i = 0; i < pageCount; i++ ) {
@@ -116,7 +202,11 @@ function addPagination (list) {
    })
 }
 
+
+
 // Call functions
 
+
+createSearch();
 showPage(data, 1);
 addPagination(data);
